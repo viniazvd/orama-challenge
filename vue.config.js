9d85a1b437fb102cedf0d903a4fda4dcb126ab2e
viniazvd <vinicius.azevedo@convenia.com.br>
2020-10-20 17:37:47 -0300

    :wrench: configure global styles

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: '@import "./node_modules/foundation-sites/scss/_global.scss";'
+      }
+    }
+  }
+}
