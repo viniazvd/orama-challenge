e36647c06c9ff6c982e6ff407a45aad3d10df1d8
viniazvd <vinicius.azevedo@convenia.com.br>
2020-10-20 17:29:13 -0300

    :wrench: import foundation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './containers/App.vue'
+
+// foundation setup (jquery is required)
+import 'jquery'
+import foundation from 'foundation-sites'
+
+// import 'vue-material-design-icons/styles.css'
+
+Vue.use(foundation)
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+new Vue({ render: h => h(App) }).$mount('#app')
