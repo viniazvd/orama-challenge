a955625a9f08d76273334553110209841c7e9d28
viniazvd <vinicius.azevedo@convenia.com.br>
2020-10-20 19:46:16 -0300

    :tada: :sparkles: init

---
+++ src/mixins/responsive.js
@@ -0,0 +1,13 @@
+export default {
+  inject: ['getIsXsMobile', 'getIsMobile'],
+
+  computed: {
+    isXsMobile () {
+      return this.getIsXsMobile()
+    },
+
+    isMobile () {
+      return this.getIsMobile()
+    }
+  }
+}
---
+++ src/mixins/setupResponsive.js
@@ -0,0 +1,54 @@
+export default {
+  data () {
+    return {
+      xsMobileMedia: window.matchMedia('screen and (max-width: 576px)'),
+      mobileMedia: window.matchMedia('screen and (max-width: 1023px)'),
+      isXsMobile: false,
+      isMobile: false
+    }
+  },
+
+  methods: {
+    setBreakpoint () {
+      this.isMobile = this.mobileMedia && this.mobileMedia.matches
+      this.isXsMobile = this.xsMobileMedia && this.xsMobileMedia.matches
+    },
+
+    getIsXsMobile () {
+      return this.isXsMobile
+    },
+
+    getIsMobile () {
+      return this.isMobile
+    },
+
+    onOrientationChange () {
+      window.addEventListener('resize', () => {
+        const matchMedia = window.matchMedia || (() => {})
+        if ((matchMedia('(orientation: portrait)') || {}).matches) {
+          setTimeout(() => window.scrollTo(0, 1), 400)
+        }
+      }, { once: true })
+    }
+  },
+
+  beforeDestroy () {
+    this.mobileMedia.removeListener(this.setBreakpoint)
+    this.xsMobileMedia.removeListener(this.setBreakpoint)
+    window.removeEventListener('orientationchange', this.onOrientationChange)
+  },
+
+  mounted () {
+    this.mobileMedia.addListener(this.setBreakpoint)
+    this.xsMobileMedia.addListener(this.setBreakpoint)
+    window.addEventListener('orientationchange', this.onOrientationChange)
+    this.setBreakpoint()
+  },
+
+  provide () {
+    return {
+      getIsXsMobile: this.getIsXsMobile,
+      getIsMobile: this.getIsMobile
+    }
+  }
+}
