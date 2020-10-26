bd825832c2f602506f98580c76d0c0632c4bba58
viniazvd <vinicius.azevedo@convenia.com.br>
2020-10-26 18:25:11 -0300

    :art: :lipstick: replace mock with definitive states and adjust layout/behaviors

--- src/components/LeftFilters.vue
+++ src/components/LeftFilters.vue
@@ -1,63 +1,102 @@
 <template>
-  <div class="left-filters cell radius padding-1">
-    <input type="text" class="search">
+  <div class="left-filters radius padding-1">
+    <md-field class="search">
+      <md-input placeholder="Buscar fundo por nome" v-model="search" />
+      <search-icon class="icon" />
+    </md-field>
 
-    <div class="controls padding-2 grid-x align-spaced background-primary">
+    <div v-if="!isMobile" class="controls padding-2 grid-x align-spaced background-primary">
       <c-range
         class="minimal-application"
 
         title="Aplicação mínima"
-        footer="Até R$ 20.000,00"
+        :footer="`Até ${toMoney(minimalApplication)}`"
 
         :min="0"
-        :max="100"
-        :value="step"
-        @input="value => step = value"
+        :max="500000"
+        :value="minimalApplication"
+        @input="value => minimalApplication = value"
       />
 
-      <span class="risk-profile">filter2</span>
+      <c-range-color class="risk-profile" />
 
       <c-range
         class="rescue-deadline"
 
         title="Prazo de resgate"
-        footer="Até 30 dias úteis"
+        :footer="`Até ${rescueDeadline} dias úteis`"
 
         :min="0"
-        :max="100"
-        :value="step"
-        @input="value => step = value"
+        :max="270"
+        :value="rescueDeadline"
+        @input="value => rescueDeadline = value"
       />
 
     </div>
 
-    <div class="time-limit margin-top-1">Horário limite da aplicação: 12:00</div>
+    <div class="time-limit">Horário limite da aplicação: 12:00</div>
   </div>
 </template>
 
 <script>
+import { toMoney } from '../utils/toMoney'
+import responsive from '../mixins/responsive'
+
 import CRange from './common/CRange'
+import CRangeColor from './common/CRangeColor'
+import SearchIcon from './common/icons/Search'
 
 export default {
   name: 'left-filters',
 
-  components: { CRange },
+  mixins: [responsive],
+
+  components: { CRange, CRangeColor, SearchIcon },
 
   data () {
     return {
-      step: 10
+      search: '',
+      rescueDeadline: 0,
+      minimalApplication: 0
     }
-  }
+  },
+
+  watch: {
+    search (v) {
+      this.$emit('sync:search', v)
+    }
+  },
+
+  methods: { toMoney }
 }
 </script>
 
 <style lang="scss">
 .left-filters {
+  order: 1;
+  box-shadow: $box-shadow;
   height: unset !important;
   background-color: white;
 
+  &.cell { margin-top: 20px !important; }
+
+  & > .search {
+    padding-top: 0 !important;
+    margin: 0 0 20px !important;
+    min-height: auto !important;
+    @include responsive (tablet, desktop) { max-width: 453px; }
+  }
+
   & > .controls {
     background: #FAFAFA;
+    align-items: center;
+    justify-content: space-between;
+
+    @media only screen and (max-width: #{map-get($responsive_breakpoints, desktop) - 1px}) {
+      flex-direction: column !important;
+
+      & > div:not(:last-child) { margin-bottom: 25px; }
+    }
 
     & > .minimal-application, .rescue-deadline { max-width: 190px; }
 
@@ -67,8 +106,8 @@ export default {
   & > .time-limit {
     color: #8D8D8D;
     font-size: 11px;
-    font-weight: 400; // regular
-    font-family: 'Roboto', sans-serif;
+    margin-top: 10px;
+    font-family: $font-family;
   }
 }
 </style>
