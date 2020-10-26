9a1ab75923967229bdd799f12891e93078240410
viniazvd <vinicius.azevedo@convenia.com.br>
2020-10-26 18:20:32 -0300

    :wrench: init vue material

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './containers/App.vue'
 
-// foundation setup (jquery is required)
-import 'jquery'
-import foundation from 'foundation-sites'
-
 // import 'vue-material-design-icons/styles.css'
 
-Vue.use(foundation)
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
