00d0a01f4cbb37ccf21ea29af25c7a2c59dce9b9
viniazvd <vinicius.azevedo@convenia.com.br>
2020-10-31 03:54:06 -0300

    :wrench: :bug: IE issue #explicitly transpile vue-material

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  // explicitly transpile vue-material
+  // https://github.com/vuematerial/vue-material/issues/1526
+  transpileDependencies: ['vue-material', 'vue-material-design-icons'],
+
   // https://github.com/neutrinojs/webpack-chain/tree/v4#getting-started
   chainWebpack (config) {
     // Set up all the aliases we use in our app.
