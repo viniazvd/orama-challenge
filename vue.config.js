86a0eaa5ca4f0d99cf084718b0a3548a4f2aab89
viniazvd <vinicius.azevedo@convenia.com.br>
2020-10-31 02:04:39 -0300

    :wrench: scss config

--- vue.config.js
+++ vue.config.js
@@ -3,18 +3,14 @@ module.exports = {
   chainWebpack (config) {
     // Set up all the aliases we use in our app.
     config.resolve.alias.clear().merge(require('./aliases.config').webpack)
+
+    config.resolve.extensions.add('.scss')
   },
 
   // https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
   css: {
-    sourceMap: true,
     loaderOptions: {
-      sass: {
-        prependData: `
-        // @import "./node_modules/foundation-sites/scss/foundation.scss";
-        @import "./src/styles/index.scss";
-        `
-      }
+      sass: { prependData: '@import "@styles/index.scss";' }
     }
   }
 }
