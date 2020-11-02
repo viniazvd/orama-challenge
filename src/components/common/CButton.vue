8953b2926f91193e2c0bba9145f6d8ef9274e332
viniazvd <vinicius.azevedo@convenia.com.br>
2020-11-02 19:16:20 -0300

    :tada: init

---
+++ src/components/common/CButton.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="c-button" :style="buttonStyles">
+    <span class="text" :style="textStyles">{{ text }}</span>
+
+    <component v-if="icon" :is="icon" class="icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'c-button',
+
+  props: {
+    fullWidth: Boolean,
+
+    icon: String,
+    bgColor: String,
+
+    text: String,
+    textColor: String,
+
+    borderColor: String
+  },
+
+  computed: {
+    buttonStyles () {
+      return {
+        'background-color': this.bgColor,
+        'border-color': this.borderColor,
+        width: this.fullWidth ? '100%' : 'auto'
+      }
+    },
+
+    textStyles () {
+      return { color: this.textColor }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.c-button {
+  background-color: white;
+  border: 1px solid transparent;
+
+  height: 40px;
+  border-radius: 3px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & > .text {
+    font-size: 14px;
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+
+  & > .icon {
+    display: flex;
+    margin-left: 5px;
+    justify-content: center;
+  }
+}
+</style>
---
+++ src/components/table/RowMobile.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="row-wrapper-mobile" @click="redirect">
+    <div class="row">
+      <span class="title">{{ row.simple_name }}</span>
+      <span class="risk-color" :style="color" />
+    </div>
+
+    <div class="row sub-title">{{ row.specification.fund_macro_strategy.name }} | {{ row.specification.fund_main_strategy.name }}</div>
+
+    <div class="row">
+      <span class="label">Data da cota:</span>
+      <span class="value">{{ formatDate(row.quota_date) }}</span>
+    </div>
+
+    <div class="row">
+      <span class="label">Rentabilidade 12 meses:</span>
+      <span class="value">{{ toPercentage(row.profitabilities.m12) + '%'}}</span>
+    </div>
+
+    <div class="row">
+      <span class="label">Aplicação mínima:</span>
+      <span class="value">{{ toMoney(row.operability.minimum_initial_application_amount) }}</span>
+    </div>
+
+    <div class="row">
+      <span class="label">Cotização do resgate:</span>
+      <span class="value">{{ row.operability.retrieval_quotation_days_str }}</span>
+    </div>
+
+    <div class="row">
+      <c-button
+        full-width
+        class="more-details"
+        text="Mais detalhes"
+        text-color="#25B7BA"
+        border-color="#25B7BA"
+      />
+
+      <c-button
+        full-width
+        icon="reply"
+        class="reply"
+        text="Aplicar"
+        text-color="white"
+        bg-color="#25B7BA"
+        @click.native.stop="apply"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import toMoney from '@utils/toMoney'
+import formatDate from '@utils/formatDate'
+import toPercentage from '@utils/toPercentage'
+
+import levels from '../../utils/levels'
+
+import CButton from '@components:common/CButton'
+
+export default {
+  name: 'table-row-mobile',
+
+  components: { CButton },
+
+  props: {
+    row: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+    toMoney,
+    formatDate,
+    toPercentage,
+
+    redirect () {
+      if (!this.row.id) return
+
+      const url = `https://www.orama.com.br/investimentos/fundos-de-investimento/${this.row.id}`
+      window.open(url, '_blank')
+    },
+
+    apply () {
+      console.log('apply')
+    }
+  },
+
+  computed: {
+    color () {
+      const level = this.row.specification.fund_risk_profile.score_range_order
+
+      return {
+        'background-color': levels[level]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.row-wrapper-mobile {
+  width: 100%;
+  cursor: pointer;
+  padding: 25px 15px 20px;
+  background-color: white;
+  border-bottom: solid 1px #eff0f2;
+
+  &:hover { background: #e6fcfc; }
+
+  & > .row {
+    display: flex;
+    justify-content: space-between;
+
+    &:not(:last-child) { margin-bottom: 15px; }
+
+    & > .label, .title {
+      flex: 50%;
+
+      @include text;
+      font-weight: 700;
+      color: #505152 !important;
+    }
+
+    & > .value {
+      flex: 50%;
+
+      @include text;
+      font-size: 12px;
+      text-align: right;
+      font-weight: normal;
+    }
+
+    & > .risk-color {
+      height: 14px;
+      width: 14px;
+      border-radius: 3px;
+    }
+
+    & > .more-details {
+      font-weight: 600;
+      box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2);
+    }
+
+    & > .reply {
+      margin-left: 10px;
+
+      & > .icon > svg {
+        background: white;
+
+        & > path { fill: #25B7BA; }
+      }
+    }
+  }
+
+  & > .sub-title {
+    @include text;
+    font-size: 12px;
+    font-weight: normal;
+  }
+}
+</style>
